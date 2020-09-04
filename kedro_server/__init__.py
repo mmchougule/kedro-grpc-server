ee16499f400146fe31923869fef5f1a8c0e6cb59
Mayur Chougule <mayurchougule1@gmail.com>
2020-09-04 17:07:35 -0400

    Cleanup

--- kedro_server/__init__.py
+++ kedro_server/__init__.py
@@ -1,6 +1,6 @@
 """
-kedro_server
-Kedro-Server, a Kedro plugin that surfaces a gRPC server for triggering and monitoring runs using Flask
+kedro_grpc_server
+Kedro-grpc-Server, a Kedro plugin that surfaces a gRPC server for triggering and monitoring runs using gRPC
 """
 
 __version__ = "0.1.0"
